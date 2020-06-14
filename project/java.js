b48254f37f2844f260a389a8cb7a7e5b4cfeb0d8
Noah Gillespie <59939752+Suwanee1@users.noreply.github.com>
2020-06-14 14:17:54 -0400

    Javascript update v2

--- project/java.js
+++ project/java.js
@@ -1,14 +1,15 @@
-$("#slideshow > div:gt(0)").hide();
+//$("#slideshow > div:gt(0)").hide();
 
 setInterval(function() {
-  $('#slideshow > div:first')
+  $('#slide > div:first')
     .fadeOut(1000)
     .next()
     .fadeIn(1000)
     .end()
-    .appendTo('#slideshow');
+    .appendTo('#slide');
 }, 5000);
 
-$(function () {
-  $("#accordian").accordian();
+
+$(function() {
+  $("#accordion").accordion();
 })
